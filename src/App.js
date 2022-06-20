d160e68d002b801c4dbc0d3fec776e56139b605b
Karicharla GNV Swamy Naidu <ganeshkaricherla@gmail.com>
2022-06-20 15:51:42 +0530

    hello finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Template1 from "./Templates/Template1/Template";
-import Template2 from "./Templates/Template2/Template";
-import Template3 from "./Templates/Template3/Template";
-function App() {
 
+function App() {
   return (
     <>
-      <Template3 />
+      <Template1 />
     </>
   );
 }
--- src/Template1/components/Project/Project.jsx
+++ src/Template1/components/Project/Project.jsx
@@ -1,31 +1,29 @@
-import { Link } from 'react-router-dom';
-import './Project.css';
-import { useState } from 'react';
+import "./Project.css";
 
 function Project({ project, showProject }) {
-    return (
-        <div className="project-content-item">
-            <div
-                className="project-card"
-                style={{
-                    backgroundImage: `url(${project.imageURL})`,
-                }}
-            >
-                <div className="project-card-overlay">
-                    <h1 className="project-card-overlay-title">{project.title}</h1>
-                    <p className="project-card-overlay-content">{project.content}</p>
-                    <button
-                        className="project-card-readme-button"
-                        onClick={() => {
-                            showProject(project.id);
-                        }}
-                    >
-                        Readmore
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="project-content-item">
+      <div
+        className="project-card"
+        style={{
+          backgroundImage: `url(${project.imageURL})`,
+        }}
+      >
+        <div className="project-card-overlay">
+          <h1 className="project-card-overlay-title">{project.title}</h1>
+          <p className="project-card-overlay-content">{project.content}</p>
+          <button
+            className="project-card-readme-button"
+            onClick={() => {
+              showProject(project.id);
+            }}
+          >
+            Readmore
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Project;
